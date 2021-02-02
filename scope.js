350a7ce107bb7d4a0c251831a9a522b456e852a2
sunjid-git <sunjid.info@gmail.com>
2021-02-02 19:55:04 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20;
+
+function sum(first,second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = 'happy';
+        mood = 'tapi';
+        mood = 'safi';
+        mood = 'lafi';
+        // console.log(mood);
+    }
+  
+    let day= 'friday';
+    console.log(day);
+    return result;
+    
+}
+const output = sum (3,7);
+// console.log(bonus);
+
+// console.log(output);

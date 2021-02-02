04a697ef050ba849e5cf06d8b33c65fdb54904f8
sunjid-git <sunjid.info@gmail.com>
2021-02-02 16:57:17 +0600

    true false

---
+++ trueFalse.js
@@ -0,0 +1,20 @@
+// Falsy value
+// 0
+// ""
+// undefined
+// null
+// false
+
+// truthy value
+// '0', ' ', [], 'false'
+
+
+// const name = 'sunjid';
+let name = 'false';
+
+if(name){
+    console.log("true");
+}
+else{
+    console.log('false');
+}
